48e005f33421e1787a8baa22a2f7b3077baf6137
JUDY LEE <ljh.social@ewhain.net>
2025-08-07 22:16:00 +0900

    initial commit

---
+++ T8-NoFrank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  T8-NoFrank
+//
+//  Created by 이주현 on 8/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
